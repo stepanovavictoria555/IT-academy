59b1daabd21a79f0fbc333ecdd85ea7da8d00c12
Stepanova Victoria <stepanovavika_01071988@mail.ru>
2022-09-14 21:32:00 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] mas = new int[n][n];
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; i < mas[i].length; i++) {
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
